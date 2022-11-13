9dd222415d1639bca80e449be8fbe1c5d767a404
babymonie <62741287+babymonie@users.noreply.github.com>
2022-11-13 20:55:33 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,10 @@
-const config = {
-  dataPath: './databases',
-};
 
-module.exports = config;
+require('dotenv').config();
+var os = require('os');
+const userInfo = os.userInfo();
+const defaultDataPath = `C:\\Users\\${userInfo.username}\\AppData\\Roaming\\npm\\node_modules\\roso-db`;
+const dataPath = process.env.DATA_PATH || defaultDataPath;
+
+module.exports = {
+    dataPath,
+}
